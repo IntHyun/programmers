899e87911506ea6ff0605043a28efa647d98d73b
정수현 <68059880+IntHyun@users.noreply.github.com>
2023-04-09 22:16:31 +0900

    [level 0] Title: 약수 구하기, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120897. 약수 구하기/약수 구하기.js
@@ -0,0 +1,15 @@
+function solution(n) {
+  let answer = [];
+
+  for (let i = 1; i <= Math.sqrt(n); i++) {
+    if (n % i === 0) {
+      answer.push(i);
+      if (i !== n / i) {
+        answer.push(n / i);
+      }
+    }
+  }
+
+  answer.sort((a, b) => a - b);
+  return answer;
+}
