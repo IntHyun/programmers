e710d6e652b3f30eb5b7ca301d904e2245d688f2
정수현 <68059880+IntHyun@users.noreply.github.com>
2023-06-03 16:11:03 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv2/12939. 최댓값과 최솟값/최댓값과 최솟값.js
+++ 프로그래머스/lv2/12939. 최댓값과 최솟값/최댓값과 최솟값.js
@@ -1,14 +1,5 @@
     function solution(s) {
-      let answer = ""
+      let answer = s.split(" ")
 
-      let stringToArray = s.split(" ")
-      let loop = stringToArray.length
-
-      let arr = []
-
-      for (let i = 0; i < loop; i++) {
-        arr.push(Math.min.apply(null, stringToArray))
-        arr.push(Math.max.apply(null, stringToArray))
-        return arr.join(" ")
-      }
+      return `${Math.min(...answer)} ${Math.max(...answer)}`
     }
