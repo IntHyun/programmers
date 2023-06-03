8d206b9d148702a6c460bb4831f402a4bd774eae
정수현 <68059880+IntHyun@users.noreply.github.com>
2023-06-03 17:20:40 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 0.05 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/lv2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.js
+++ 프로그래머스/lv2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.js
@@ -1,11 +1,11 @@
-    function solution(s) {
-      let answer = []
-      let toArr = s.split(" ")
+function solution(s) {
+  let answer = [];
+  let toArr = s.split(" ");
 
-      for (const item of toArr) {
-        let test = item.charAt(0).toUpperCase() + item.substring(1).toLowerCase()
-        answer.push(test)
-      }
-        
-      return answer.join(" ")
-    }
+  for (const item of toArr) {
+    let toUpper = item.charAt(0).toUpperCase() + item.slice(1).toLowerCase();
+    answer.push(toUpper);
+  }
+
+  return answer.join(" ");
+}
